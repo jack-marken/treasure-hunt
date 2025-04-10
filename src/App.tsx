c4f322228641f24a3ee93c2c43c7a829aace9912
jack-marken <204086569+jack-marken1@users.noreply.github.com>
2025-04-10 21:48:47 +1000

    edit frst page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  // const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-    <h1 class="bg-indigo-500">HEYYYYYY</h1>
-    </>
-  )
+    <div className="min-h-screen flex justify-center bg-indigo-200">
+        <div className="bg-indigo-600">hi</div>
+        <div className="aspect-video max-w-sm inline-block bg-white">
+            <div className="relative -left-10 aspect-square rounded-full w-12 bg-orange-500"></div>
+            <div className="relative -right-10 aspect-square rounded-full w-24 bg-purple-500"></div>
+            <div className="relative aspect-video m-6 p-6 w-auto sm:w-96 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5 backdrop-blur-lg">
+                <div>
+                    <div className="text-xl font-medium text-black">Big Text</div>
+                    <p className="text-gray-500">Small paragraph text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
 }
-
-export default App
