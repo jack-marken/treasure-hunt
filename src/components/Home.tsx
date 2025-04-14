fcca0e04391dea8f57a55dd3b5d5dd52e6f35b4c
jack-marken <204086569+jack-marken1@users.noreply.github.com>
2025-04-14 21:21:03 +1000

    new home page visuals

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,26 +18,20 @@ export default function App() {
 
   return (
     <div className="min-h-screen">
-        <header className="h-32 py-2 text-white text-center bg-emerald-800">
+        <header className="pt-10 py-4 text-center text-emerald-800 text-xl">
             Treasure Hunt
         </header>
-        <div className="-mt-6 pt-20 bg-white rounded-t-[24px] h-svh">
-            <div className="flex flex-col bg-gray-200">
-                <div className="flex overflow-x-scroll pb-10 hide-scrollbar">
-                    <div className="flex flex-nowrap ml-10">
-                        {adventures.map((a: any) => (
-                        <div className="inline-block px-3">
-                            <div className="py-4 px-6 flex justify-between h-96 w-64 rounded-[18px] shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                              <h1>{a.title}</h1>
-                              <p>{a.is_completed.toString()}</p>
-                            </div>
-                        </div>
-                        ))}
-                    </div>
+        <div className="h-svh pt-12">
+            {adventures.map((a: any) => (
+            <div key={a.id} className="flex justify-center">
+                <div className="flex justify-between mb-12 mx-4 py-4 px-6 w-full h-36 rounded-lg bg-gray-200 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                  <h1>{a.title}</h1>
+                  <p>{a.is_completed.toString()}</p>
                 </div>
             </div>
+            ))}
         </div>
-        <div className="bg-red-100 h-64">
+        <div className="bg-emerald-700 h-64">
         </div>
     </div>
   );
