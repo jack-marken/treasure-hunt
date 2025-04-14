1c51609284ecad8350684b31aef40cb7a836c783
jack-marken <204086569+jack-marken1@users.noreply.github.com>
2025-04-14 21:51:28 +1000

    Remove commented code

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router"
 import './index.css'
 import Home from './Home.tsx'
 import Adventure from './Adventure.tsx'
 
-        // <Route index element={<App />} />
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
