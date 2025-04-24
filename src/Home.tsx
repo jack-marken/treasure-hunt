27f1ef5ae658aa1ce547bb72bf6b62f618b62f2e
jack-marken <204086569+jack-marken1@users.noreply.github.com>
2025-04-24 14:26:49 +1000

    New layout

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,23 +1,35 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import supabase from "./supabaseClient";
+import { BsMap } from "react-icons/bs";
+import { RiTreasureMapFill } from "react-icons/ri";
+import { MdOutlineQuestionMark } from "react-icons/md";
 
 export default function App() {
   return (
-    <div className="min-h-screen">
-        <header className="pt-10 py-4 text-center text-emerald-800 text-xl">
-            Treasure Hunt
-        </header>
-        <div className="h-svh pt-12">
-            <div className="flex justify-center">
-                <div className="flex justify-between mb-12 mx-4 py-4 px-6 w-full h-36 rounded-lg bg-gray-200 hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                    <Link to="adventure">
-                    </Link>
-                </div>
-            </div>
+    <div className="h-dvh pt-3 px-6 bg-emerald-800">
+      <header className="fixed top-8 right-8 left-8 text-white text-4xl">
+        <ul className="flex justify-between">
+          <li><RiTreasureMapFill /></li>
+          <li>1/2</li>
+          <li><MdOutlineQuestionMark /></li>
+        </ul>
+      </header>
+      <div className="">
+        <h1>HEY</h1>
+        <div className="flex justify-center">
         </div>
-        <div className="bg-emerald-700 h-64">
-        </div>
+      </div>
+      <div className="fixed right-8 bottom-8 left-8">
+        <button className="flex items-center justify-center w-full h-16 rounded-md bg-gray-200 text-xl shadow-solid">
+          I'VE GOT IT
+        </button>
+      </div>
     </div>
   );
 }
+
+// <div className="flex justify-between mb-12 mx-4 py-4 px-6 w-full h-36 rounded-lg bg-gray-200 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+//   <Link to="adventure">
+//   </Link>
+// </div>
